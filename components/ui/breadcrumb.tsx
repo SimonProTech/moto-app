ea0a305a34aec8df7c3daa0ab98ceb4b7b7488be
Szymon <szymon98181@gmail.com>
2025-12-23 20:04:15 +0100

    fixed breadcrumb

--- components/ui/breadcrumb.tsx
+++ components/ui/breadcrumb.tsx
@@ -13,7 +13,7 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
     <ol
       data-slot="breadcrumb-list"
       className={cn(
-        "text-muted-foreground flex flex-wrap items-center gap-1.5 text-md break-words sm:gap-3,5",
+        "text-muted-foreground flex flex-wrap items-center gap-1.5 text-md break-words sm:gap-4",
         className,
       )}
       {...props}
